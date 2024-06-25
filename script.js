1d3b9b3f1825c7bfd3e18d9e475312b4bb3086ec
Kirk J-Son Matic <kirkmatic1@gmail.com>
2024-06-25 21:33:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,84 @@
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeapon = 0;
-let fighting;
-let monsterHealth;
-let inventory = ["stick"];
-
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText =document.querySelector("#monsterHealth");
-const weapons = [
-  { name: 'stick', power: 5 },
-  { name: 'dagger', power: 30 },
-  { name: 'claw hammer', power: 50 },
-  { name: 'sword', power: 100 }
-];
 const monsters = [
   {
     name: "slime",
     level: 2,
-    health: 15
+    health: 15,
+    image: "images/slime.jpg"
   },
   {
     name: "fanged beast",
     level: 8,
-    health: 60
+    health: 60,
+    image: "images/beast.jpg"
   },
   {
     name: "dragon",
     level: 20,
-    health: 300
+    health: 300,
+    image: "images/dragon.jpg"
   }
-]
+];
+
 const locations = [
     {
         name: "town square",
         "button text": ["Go to store", "Go to cave", "Fight dragon"],
         "button functions": [goStore, goCave, fightDragon],
-        text: "You are in the town square. You see a sign that says \"Store\"."
+        text: "You are in the town square. You see a sign that says \"Store\".",
+        image: "images/town.jpg"
     },
     {
         name: "store",
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
-        text: "You enter the store."
+        text: "You enter the store.",
+        image: "images/store.jpg"
     },
     {
         name: "cave",
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
-        text: "You enter the cave. You see some monsters."
+        text: "You enter the cave. You see some monsters.",
+        image: "images/cave.jpg"
     },
     {
         name: "fight",
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
-        text: "You are fighting a monster."
+        text: "You are fighting a monster.",
+        image: "images/fight.jpg"
     },
     {
         name: "kill monster",
         "button text": ["Go to town square", "Go to town square", "easterEgg"],
         "button functions": [goTown, goTown, easterEgg],
-        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+        image: "images/kill.jpg"
     },
     {
         name: "lose",
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
-        text: "You die. ☠️"
+        text: "You die. ☠️",
+        image: "images/lose.jpg"
     },
     { 
         name: "win", 
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
         "button functions": [restart, restart, restart], 
-        text: "You defeat the dragon! YOU WIN THE GAME! 🎉" 
+        text: "You defeat the dragon! YOU WIN THE GAME! 🎉", 
+        image: "images/win.jpg"
     },
     {
         name: "easter egg",
         "button text": ["2", "8", "Go to town square?"],
         "button functions": [pickTwo, pickEight, goTown],
-        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
+        image: "images/easter_egg.jpg"
     }
 ];
 
-// initialize buttons
+// Initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
@@ -104,6 +92,7 @@ function update(location) {
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
   text.innerText = location.text;
+  locationImage.src = location.image;
 }
 
 function goTown() {
@@ -182,6 +171,7 @@ function goFight() {
   monsterStats.style.display = "block";
   monsterName.innerText = monsters[fighting].name;
   monsterHealthText.innerText = monsterHealth;
+  monsterImage.src = monsters[fighting].image;
 }
 
 function attack() {
@@ -278,15 +268,3 @@ function pick(guess) {
     health -= 10;
     healthText.innerText = health
         if(health <= 0){
-            lose()
-        }
-  }
-}
-
-// try functions
-
-const main = document.querySelector("main");
-
-
-
-
